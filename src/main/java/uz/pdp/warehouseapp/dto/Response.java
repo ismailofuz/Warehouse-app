cfff4a794227e5ad1e46ad10d239f4cd8287e2af
Javokhirbekrakhimov <javohirbekrakhimov@gmail.com>
2022-02-16 13:53:47 +0500

    commit

--- src/main/java/uz/pdp/warehouseapp/dto/Response.java
+++ src/main/java/uz/pdp/warehouseapp/dto/Response.java
@@ -1,14 +1,10 @@
 package uz.pdp.warehouseapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-@Getter
+@Data
 public class Response {
     private String message;
     private boolean success;
