1c7cbcbfc24e07b55815d0ef9e6d5ffc062674bf
Patrick96 <gayratjonkamolov96@gmail.com>
2022-02-17 22:46:29 +0500

    index.html, user_form.html, users.html,yaratildi UserController.java va UserService.java fayllari toliq emas hozircha

--- src/main/java/uz/pdp/warehouseapp/service/UserService.java
+++ src/main/java/uz/pdp/warehouseapp/service/UserService.java
@@ -1,7 +1,23 @@
 package uz.pdp.warehouseapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.pdp.warehouseapp.entity.User;
+import uz.pdp.warehouseapp.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> listAll() {
+        return userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+
+    }
 }
