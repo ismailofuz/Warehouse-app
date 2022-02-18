7217f3beff97ae2605f31fe7f44dda759bd84c3e
Javokhirbekrakhimov <javohirbekrakhimov@gmail.com>
2022-02-18 22:13:44 +0500

    Finished category and measurement

--- src/main/java/uz/pdp/warehouseapp/service/CategoryService.java
+++ src/main/java/uz/pdp/warehouseapp/service/CategoryService.java
@@ -7,6 +7,7 @@ import uz.pdp.warehouseapp.entity.Category;
 import uz.pdp.warehouseapp.repository.CategoryRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -45,7 +46,7 @@ public class CategoryService {
     }
   public boolean findCategoryByName(String name){
       for (Category category : categoryRepository.findAll()) {
-          if(category.getName().trim().toLowerCase().equals(name.trim().toLowerCase()))
+          if(category.getName().trim().equalsIgnoreCase(name.trim()))
               return true;
       }
       return false;
@@ -58,8 +59,9 @@ public class CategoryService {
         Response response=new Response();
         boolean hasName=false;
         for (Category category1 : categoryRepository.findAll()) {
-            if(category1.getName().trim().toLowerCase().equals(category.getName().trim().toLowerCase())&&!category1.getId().equals(category.getId())){
-                hasName=true;
+            if (category1.getName().trim().equalsIgnoreCase(category.getName().trim()) && !category1.getId().equals(category.getId())) {
+                hasName = true;
+                break;
             }
         }
         if(!hasName) {
@@ -69,6 +71,21 @@ public class CategoryService {
             return response;
         }
         response.setMessage("This name already exist");
+        return response;
+    }
+
+    public Response deleteMeasurement(Integer id) {
+        Response response=new Response();
+        Optional<Category> byId = categoryRepository.findById(id);
+        if (byId.isPresent()) {
+            Category category = byId.get();
+            category.setActive(false);
+            categoryRepository.save(category);
+            response.setMessage("This category is no active");
+            response.setSuccess(true);
+            return response;
+        }
+        response.setMessage("Not found category");
         return response;
     }
 }
