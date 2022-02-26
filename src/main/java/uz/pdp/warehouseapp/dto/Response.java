4e95e471895c7e5533578b03f19f0dadcea55167
Javokhirbek Rakhimov <javohirbekrakhimov@gmail.com>
2022-02-26 17:44:00 +0500

    show product

--- src/main/java/uz/pdp/warehouseapp/dto/Response.java
+++ src/main/java/uz/pdp/warehouseapp/dto/Response.java
@@ -10,4 +10,12 @@ import lombok.*;
 public class Response {
     private String message;
     private boolean success;
+
+    public Response(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    private Object object;
+
 }
